2339e3e2501c7254ed14008d1628e6ad5fb39786
= <=>
2022-06-04 15:45:20 +0200

    working lstm

--- src/lstm.py
+++ src/lstm.py
@@ -4,6 +4,121 @@ import torch.autograd as autograd
 import torch.nn.functional as F
 
 from typing import Optional, Tuple
+from torch import Tensor
+
+
+class LSTM(torch.nn.Module):
+    """ 
+    """
+
+    def __init__(
+        self,
+        hidden_size,
+        input_shape,
+        num_layers=1,
+        bias=True,
+        dropout=0.0,
+        re_init=True,
+        bidirectional=False,
+    ):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.bias = bias
+        self.dropout = dropout
+        self.re_init = re_init
+        self.bidirectional = bidirectional
+        self.reshape = False
+
+        # Computing the feature dimensionality
+        if len(input_shape) > 3:
+            self.reshape = True
+        self.fea_dim = float(torch.prod(torch.tensor(input_shape[2:])))
+        self.batch_size = input_shape[0]
+        self.rnn = self._init_layers()
+
+        if self.re_init:
+            rnn_init(self.rnn)
+
+    def _init_layers(self):
+        """Initializes the layers of the liGRU."""
+        rnn = torch.nn.ModuleList([])
+        current_dim = self.fea_dim
+
+        for i in range(self.num_layers):
+            rnn_lay = LSTM_Layer(
+                current_dim,
+                self.hidden_size,
+                self.num_layers,
+                self.batch_size,
+                dropout=self.dropout,
+                bidirectional=self.bidirectional,
+            )
+            rnn.append(rnn_lay)
+
+            if self.bidirectional:
+                current_dim = self.hidden_size * 2
+            else:
+                current_dim = self.hidden_size
+        return rnn
+
+    def forward(self, x, hx: Optional[Tensor] = None):
+        """Returns the output of the liGRU.
+        Arguments
+        ---------
+        x : torch.Tensor
+            The input tensor.
+        hx : torch.Tensor
+            Starting hidden state.
+        """
+        # Reshaping input tensors for 4d inputs
+        if self.reshape:
+            if x.ndim == 4:
+                x = x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3])
+
+        # run lstm
+        output, hh, ct = self._forward_lstm(x, hx=hx)
+
+        return output, hh, ct
+
+    def _forward_lstm(self, x, hx: Optional[Tensor]):
+        """Returns the output of the vanilla liGRU.
+        Arguments
+        ---------
+        x : torch.Tensor
+            Input tensor.
+        hx : torch.Tensor
+        """
+        h = []
+        c = []
+        if hx is not None:
+            if self.bidirectional:
+                hx[0] = hx[0].reshape(
+                    self.num_layers, self.batch_size * 2, self.hidden_size
+                )
+
+                hx[1] = hx[1].reshape(
+                    self.num_layers, self.batch_size * 2, self.hidden_size
+                )
+        # Processing the different layers
+        for i, lstm_lay in enumerate(self.rnn):
+            if hx is not None:
+                x, ct = lstm_lay(x, hx=(hx[0][i], hx[1][i]))
+            else:
+                x, ct = lstm_lay(x, hx=None)
+            h.append(x[:, -1, :])
+            c.append(ct[:, -1, :])
+        h = torch.stack(h, dim=1)
+        c = torch.stack(c, dim=1)
+        if self.bidirectional:
+            h = h.reshape(h.shape[1] * 2, h.shape[0], self.hidden_size)
+            c = c.reshape(c.shape[1] * 2, c.shape[0], self.hidden_size)
+        else:
+            h = h.transpose(0, 1)
+            c = c.transpose(0, 1)
+
+        return x, h, c
+
 
 
 class LSTM_Layer(torch.nn.Module):
@@ -60,17 +175,24 @@ class LSTM_Layer(torch.nn.Module):
         
         # Processing time steps
         if hx is not None:
-            h, c = hx 
-            h = self._lstm_cell(w, h, c)
+            h, c, = hx 
+            h, c = self._lstm_cell(w, h, c)
         else:
-            h = self._lstm_cell(w, self.h_init, self.c_init)
+            h, c = self._lstm_cell(w, self.h_init, self.c_init)
 
         if self.bidirectional:
             h_f, h_b = h.chunk(2, dim=0)
             h_b = h_b.flip(1)
             h = torch.cat([h_f, h_b], dim=2)
 
-        return h
+            c_f, c_b = c.chunk(2, dim=0)
+            c_b = c_b.flip(1)
+            c = torch.cat([c_f, c_b], dim=2)
+
+        print(f"lay h = {h.shape}")
+        print(f"lay c = {c.shape}")
+
+        return h, c
 
 
     def _lstm_cell(self, wx: torch.Tensor, ht: torch.Tensor, ct: torch.Tensor):
@@ -84,7 +206,6 @@ class LSTM_Layer(torch.nn.Module):
         cell_state = []
 
         # Sampling dropout mask
-        # TODO: add drop mask
         drop_mask = self._sample_drop_mask(wx)
 
         # Loop over time axis
@@ -105,6 +226,8 @@ class LSTM_Layer(torch.nn.Module):
         # Stacking states
         h = torch.stack(hiddens, dim=1)
         c = torch.stack(cell_state, dim=1)
+        print(f"h = {h.shape}")
+        print(f"c = {c.shape}")
         return h, c
 
     def _sample_drop_mask(self, w):
@@ -162,6 +285,25 @@ class LSTM_Layer(torch.nn.Module):
                     )
                 ).data
 
+def rnn_init(module):
+    """This function is used to initialize the RNN weight.
+    Recurrent connection: orthogonal initialization.
+    Arguments
+    ---------
+    module: torch.nn.Module
+        Recurrent neural network module.
+    Example
+    -------
+    >>> inp_tensor = torch.rand([4, 10, 20])
+    >>> net = RNN(hidden_size=5, input_shape=inp_tensor.shape)
+    >>> out_tensor = net(inp_tensor)
+    >>> rnn_init(net)
+    """
+    for name, param in module.named_parameters():
+        if "weight_hh" in name or ".u.weight" in name:
+            nn.init.orthogonal_(param)
+
+        
 if __name__ == "__main__":
 
     hidden_size = 5
@@ -173,13 +315,18 @@ if __name__ == "__main__":
     lstm_layer = LSTM_Layer(
         input_size=input_size,
         hidden_size=hidden_size,
-        num_layers=1,
+        num_layers=5,
         batch_size=batch_size,
         dropout=0.0,
         bias=True,
         bidirectional=False
     )
 
-    out = lstm_layer(x)
-    print(out)
+    inp_tensor = torch.rand([4, 10, 20])
+    net = LSTM(input_shape=inp_tensor.shape, hidden_size=5)
+    out_tensor, h, c = net(inp_tensor)
+    print(c.shape)
+    print(h.shape)
+
+    out_tensor, h, c = net(inp_tensor, (h, c))
     
